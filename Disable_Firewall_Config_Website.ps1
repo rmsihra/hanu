366c0a65c183304f0b72fdb5a7e4634da075e4f6
Ravi Mishra <overtocloud@outlook.com>
2017-01-09 22:16:42 +0530

    ps1

---
+++ Disable_Firewall_Config_Website.ps1
@@ -0,0 +1,17 @@
+﻿
+    Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+    Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools -verbose
+
+   
+    Import-module webadministration
+
+    $SiteFolderPath = "C:\WebSite"              # Website Folder
+    $SiteAppPool = "MyAppPool"                  # Application Pool Name
+    $SiteName = "MySite"                        # IIS Site Name
+    $SiteHostName = "www.MySite.com"            # Host Header
+
+    New-Item $SiteFolderPath -type Directory
+    Set-Content $SiteFolderPath\Default.htm "<h1>Hello IIS</h1>"
+    New-Item IIS:\AppPools\$SiteAppPool
+    New-Item IIS:\Sites\$SiteName -physicalPath $SiteFolderPath -bindings @{protocol="http";bindingInformation=":80:"+$SiteHostName}
+    Set-ItemProperty IIS:\Sites\$SiteName -name applicationPool -value $SiteAppPool 
